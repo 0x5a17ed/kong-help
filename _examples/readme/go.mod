0b0c1edac7ba9221bf8fc6637f834dd31e72c004
Arthur `0x5a17ed` Skowronek <0x5a17ed@mailbox.org>
2026-05-03 17:35:01 +0200

    fix example
    Signed-off-by: Arthur `0x5a17ed` Skowronek <0x5a17ed@mailbox.org>

--- _examples/readme/go.mod
+++ _examples/readme/go.mod
@@ -3,13 +3,16 @@ module readme
 go 1.25.2
 
 require (
-	github.com/alecthomas/kong v1.13.0 // indirect
-	github.com/fatih/color v1.18.0 // indirect
-	github.com/mattn/go-colorable v0.1.14 // indirect
-	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/ohhfishal/kong-help v0.0.0-20251121191014-5875bfee45ed // indirect
-	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/term v0.37.0 // indirect
+	github.com/0x5a17ed/kong-help v0.0.0-00010101000000-000000000000
+	github.com/alecthomas/kong v1.15.0
 )
 
-replace github.com/ohhfishal/kong-help => ../..
+require (
+	github.com/fatih/color v1.19.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.22 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/term v0.42.0 // indirect
+)
+
+replace github.com/0x5a17ed/kong-help => ../..
--- _examples/readme/main.go
+++ _examples/readme/main.go
@@ -2,30 +2,43 @@ package main
 
 import (
 	"fmt"
-	"github.com/alecthomas/kong"
-	konghelp "github.com/ohhfishal/kong-help"
 	"os"
+
+	konghelp "github.com/0x5a17ed/kong-help"
+	"github.com/alecthomas/kong"
 )
+
+type (
+	StatusCmd struct{}
+	InitCmd   struct {
+		Quiet     bool   `short:"q" help:"Only print error and warning messages; all other output will be suppressed."`
+		Directory string `help:"If you provide a <directory>, the command is run inside it."`
+	}
+	CommitCmd struct {
+		All     bool   `aliases:"branches" help:"Automatically stage files that have been moddified and deleted, but new files you have not told Git about are not affected."`
+		Message string `short:"m" placeholder:"<msg>" help:"Use <msg> as the commit message."`
+		File    string `arg:"" default:"test.txt" type:"existingfile" help:"File to commit."`
+	}
+	PushCmd struct {
+		Repository string `arg:"" optional:"" help:"The remote rerpository that is the destination of a push operation."`
+		All        bool   `aliases:"branches" default:"false" help:"Push all branches; cannot be used with other <refspec>."`
+		Delete     bool   `short:"d" help:"All listed refs are deleted from the remote repository."`
+	}
+)
+
+func (InitCmd) Run() error   { return nil }
+func (StatusCmd) Run() error { return nil }
+func (CommitCmd) Run() error { return nil }
+func (PushCmd) Run() error   { return nil }
 
 type CMD struct {
 	Version bool   `short:"v" help:"Print the Git suite version that the git program came from."`
 	GitDir  string `type:"path" help:"Set the path to the reposity (\".git\" directory)."`
-	Status  struct {
-	} `cmd:"" default:"1" help:"Show the git status of the Git repository."`
-	Init struct {
-		Quiet     bool   `short:"q" help:"Only print error and warning messages; all other output will be suppressed."`
-		Directory string `help:"If you provide a <directory>, the command is run inside it."`
-	} `cmd:"" help:"Create an empty Git repository or reinitialize an existing one."`
-	Commit struct {
-		All     bool   `aliases:"branches" help:"Automatically stage files that have been moddified and deleted, but new files you have not told Git about are not affected."`
-		Message string `short:"m" placeholder:"<msg>" help:"Use <msg> as the commit message."`
-		File    []byte `arg:"" default:"test.txt" type:"existingfile" help:"File to commit."`
-	} `cmd:"" help:"Record changes to the repository."`
-	Push struct {
-		Repository string `arg:"" optional:"" help:"The remote rerpository that is the destination of a push operation."`
-		All        bool   `aliases:"branches" default:"false" help:"Push all branches; cannot be used with other <refspec>."`
-		Delete     bool   `short:"d" help:"All listed refs are deleted from the remote repository."`
-	} `cmd:"" help:"Update remote rerfs along with associated objects."`
+
+	Init   InitCmd   `cmd:"" help:"Create an empty Git repository or reinitialize an existing one."`
+	Status StatusCmd `cmd:"" default:"1" help:"Show the git status of the Git repository."`
+	Commit CommitCmd `cmd:"" help:"Record changes to the repository."`
+	Push   PushCmd   `cmd:"" help:"Update remote rerfs along with associated objects."`
 }
 
 func main() {
@@ -34,7 +47,7 @@ func main() {
 		&cli,
 		konghelp.Help(),
 		kong.Name("git"),
-		kong.Description("Subset of git commandss to showcase kong-help's output!"),
+		kong.Description("Subset of git commands to showcase kong-help's output!"),
 		kong.ConfigureHelp(kong.HelpOptions{
 			WrapUpperBound: -1, // Uses terminal width
 			// Tree: true,
